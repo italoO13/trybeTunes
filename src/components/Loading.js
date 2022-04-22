e7bdd686203402320ca3a2d98198434cca99b0f4
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-04-22 09:43:11 -0300

    Criado estilo direto no react para controlar o scroll do mouse

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,13 +3,16 @@ import styles from '../styles/loading.module.css';
 
 class Loading extends React.Component {
   render() {
+    const styleLoading = {
+      top: document.documentElement.scrollTop,
+    };
+
     return (
-      <div className={ styles.loading }>
+      <div className={ styles.loading } style={ styleLoading }>
         <div className="imgwrapper">
           <img src="https://acegif.com/wp-content/uploads/loading-11.gif" alt="git-Loading" />
         </div>
         <h1>Carregando...</h1>
-
       </div>
     );
   }
